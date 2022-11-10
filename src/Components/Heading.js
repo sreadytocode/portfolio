90a924efb4916f2ec2e348a65eee65599b691599
sreadytocode <sara.younis2303@gmail.com>
2022-11-10 00:30:22 +0000

    Added animation to image and heading

--- src/Components/Heading.js
+++ src/Components/Heading.js
@@ -1,6 +1,12 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import Background from "../Img/cloud.jpeg"
+
+const play = keyframes`
+  
+  100% { background-position: -1900px; }
+
+`
 
 const Title = styled.h1`
     padding: 1rem;
@@ -9,10 +15,17 @@ const Title = styled.h1`
     font-family: 'Lobster', cursive;
     text-align: center;
     text-shadow: 2px 2px turquoise;
-    background-image: url(${Background});
-    background-size: cover;
+    background: url(${Background}); 
     color: darkblue;
     filter: contrast(1.1);
+    animation: ${play} 50s linear infinite;
+    background-position: 0px 0px;
+    box-shadow: 0 0 8px 8px lightblue inset;
+    transform: scale(1.1);
+    background-repeat: repeat-x;
+    background-size: 100%;
+    
+    /* filter: blur(0.5px); */
     `;
 
 const Heading = ({text}) => {
--- src/Components/Image.js
+++ src/Components/Image.js
@@ -1,15 +1,34 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const BorderAnimation = keyframes`
+ 0% {
+		box-shadow: 0 5px 15px 0px rgba(0,0,0,0.6);
+		transform: translatey(0px);
+	}
+	50% {
+		box-shadow: 0 25px 15px 0px rgba(0,0,0,0.2);
+		transform: translatey(-20px);
+	}
+	100% {
+		box-shadow: 0 5px 15px 0px rgba(0,0,0,0.6);
+		transform: translatey(0px);
+	}
+`
 
 const ImageContainer = styled.div `
     float: left;
     border-radius: 50%;
     margin: 50px;
-    border: 5px dotted darkmagenta;
+    animation: ${BorderAnimation} 4s infinite linear;
+    background-repeat: repeat-x, repeat-x, repeat-y, repeat-y;
     box-shadow: 3px 2px 2px 2px lightyellow;
+    
+    
 `
 const Img = styled.img`
-    border-radius: 50%;
+    border-radius: 50%; 
+    border: 5px dotted darkmagenta;
 `
 
 const Image = () => {
